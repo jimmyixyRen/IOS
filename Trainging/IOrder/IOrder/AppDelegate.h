7c0e7f771591ba2501966429b1c6df926f68e387
rendl <developer@WutekiMac-mini.local>
2015-01-16 10:01:54 +0800

    订餐排位

---
+++ Trainging/IOrder/IOrder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOrder
+//
+//  Created by rendl on 15/1/16.
+//  Copyright (c) 2015年 BIS_developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Trainging/IOrder/IOrder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOrder
+//
+//  Created by rendl on 15/1/16.
+//  Copyright (c) 2015年 BIS_developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
